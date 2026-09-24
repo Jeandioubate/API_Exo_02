c2ddb20d78fe5654c88d79dc7cdb342759eff928
Jean Dioubate <jean.dioubate@fms-ea.com>
2026-09-24 04:16:56 +0200

    - Récupération des données de prévisions météo des 3 villes avec (requests.get) qui envoie une requête HTTP à l'API; - Conversion des réponses JSON en dico Python pour les 3 villes.

--- main.py
+++ main.py
@@ -8,3 +8,25 @@ Pour chaque ville et pour chacun des cinq jours de prévision,
 le programme recherche les températures minimales et maximales
 et affiche la température minimale et maximale de la journée.
 """
+
+# Récupération des données de prévisions météorologiques à 5 jours des trois villes
+
+# Données de Mérignac
+response_01 = requests.get(
+    "https://api.openweathermap.org/data/2.5/forecast?q=Mérignac&appid=23a4f11a83abb1b76a3a69c94f7bc24c&units=metric"
+    )
+
+# Données de Saint-Geours-de-Maremne
+reponse_02 = requests.get(
+    "https://api.openweathermap.org/data/2.5/forecast?q=Saint-Geours-de-Maremne&appid=23a4f11a83abb1b76a3a69c94f7bc24c&units=metric"
+    )
+
+# Données de Toulouse
+reponse_03 = requests.get(
+    "https://api.openweathermap.org/data/2.5/forecast?q=Toulouse&appid=23a4f11a83abb1b76a3a69c94f7bc24c&units=metric"
+    )
+
+# Conversion de la réponse JSON de l'API pour les trois villes en dictionnaire Python.
+data_mrg = response_01.json()   # Pour Mérignac
+data_sgm = reponse_02.json()    # Pour Saint-Geours-de-Maremne
+data_tls = reponse_03.json()    # Pour Toulouse
