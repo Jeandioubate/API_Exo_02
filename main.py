d8022f0002286e0f803dc7c6845d6c39c392aa01
Jean Dioubate <jean.dioubate@fms-ea.com>
2026-09-24 04:10:53 +0200

    - Import de requests; - Rédaction de la docstring

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+
+"""
+Ce programme récupère les données météorologiques de trois villes
+(Mérignac, Saint-Geours-de-Maremne et Toulouse) depuis l'API OpenWeather.
+
+Pour chaque ville et pour chacun des cinq jours de prévision,
+le programme recherche les températures minimales et maximales
+et affiche la température minimale et maximale de la journée.
+"""
