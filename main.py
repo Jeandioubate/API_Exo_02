8a2ca33b8dcd5b71e2263af7b0d9a52a9197face
Jean Dioubate <jean.dioubate@fms-ea.com>
2026-09-24 04:25:48 +0200

    - Affichage de toutes les prévisions pour les 3 villes; - Création de la liste (dates[]) qui contient les 5 dates pour lesquelles on veut afficher les prévisions météo; - Création d'un dico (city_dict{}) qui contient les données météo récupérées depuis l'API correspondant à chaque ville; - Création de la liste (cities[]) contenant les villes à traiter.

--- main.py
+++ main.py
@@ -30,3 +30,30 @@ reponse_03 = requests.get(
 data_mrg = response_01.json()   # Pour Mérignac
 data_sgm = reponse_02.json()    # Pour Saint-Geours-de-Maremne
 data_tls = reponse_03.json()    # Pour Toulouse
+
+# Affichage de toutes les prévisions pour les trois villes
+"""
+print(data_mrg["list"])
+print(data_sgm["list"])
+print(data_tls["list"])
+"""
+print()
+
+# Liste des cinq dates pour lesquelles nous voulons afficher les prévisions météorologiques.
+dates = [
+    "2026-09-24",
+    "2026-09-25",
+    "2026-09-26",
+    "2026-09-27",
+    "2026-09-28",
+]
+
+# Dictionnaire permettant d'associer chaque ville aux données météo récupérées depuis l'API.
+city_dict = {
+    "Mérignac": data_mrg,
+    "Saint-Geours-de-Maremne": data_sgm,
+    "Toulouse": data_tls
+}
+
+# Liste des villes que nous voulons traiter.
+cities = ["Mérignac", "Saint-Geours-de-Maremne", "Toulouse"]
